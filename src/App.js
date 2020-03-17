79afc1535805e54d3efb7bce2fefe0b2d5956c2d
jonathansh1115 <30495257+jonathansh1115@users.noreply.github.com>
2020-03-17 09:37:47 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// components
+import Navcol from './components/navcol'
+import Chatplace from './components/chatplace'
+
+// libraries
+import { Route } from 'react-router-dom' 
+import { Container, 
+         Row,
+         Col} from 'reactstrap'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      {/* if logged in (got token) */}
+      <Container fluid={true}>
+        <Row>
+          <Col md='3' xs='12' style={{padding:'0vw', backgroundColor:'rgb(238, 238, 238)'}}>
+            <Route path='/' component={Navcol} />
+          </Col>
+
+          <Col md='9' xs='12' style={{padding:0}}>
+            <Route path='/' component={Chatplace} />
+          </Col>
+        </Row>
+      </Container>
+
     </div>
   );
 }
